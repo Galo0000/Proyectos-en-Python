daec51549edd8c58dd50a3944978916097415841
Galo0000 <guillermogallogarcia@gmail.com>
2023-04-13 02:17:17 -0300

    Update candlestick plot.py

--- Bot cryptos/candlestick plot.py
+++ Bot cryptos/candlestick plot.py
@@ -1,42 +1,30 @@
+import sys
+sys.path.append( 'C:/Repositorios/Python' )
+sys.path.append( 'H:\Repositorios\Python no github\Bot cryptos' )
+import USERBINANCE
 import matplotlib.pyplot as plt
 import pandas as pd
+from mplfinance.original_flavor import candlestick_ohlc
+from binance.client import Client
+import numpy as np
 
-prices = pd.read_csv('ETHUSDT4hour6monthn.csv')
-prices.set_index('Date', inplace=True)
-prices = prices.iloc[0:20,:]
-#prices = pd.DataFrame({'Open': [data['Open']],
-#                       'Close': [data['Close']],
-#                       'High': [data['High']],
-#                       'Low': [data['Low']]},
-#                       index=pd.date_range("2021-01-01", periods=8, freq="d"))
+client = Client(USERBINANCE.API_KEY, USERBINANCE.API_SECRET, tld='com')
 
-#create figure
-plt.figure()
+basecoin = 'USDT'
+tradecoin = 'ETH'
+symbolTicker = tradecoin + basecoin
+klines = np.array(client.get_klines(symbol=symbolTicker, interval=Client.KLINE_INTERVAL_4HOUR,limit=50)).astype(np.float64)
 
-#define width of candlestick elements
-width = .4
-width2 = .05
 
-#define up and down prices
-up = prices[prices.Close>=prices.Open]
-down = prices[prices.Close<prices.Open]
+df = pd.DataFrame(klines[:, :5],columns=['Date','Open','High','Low','Close'])
+#df['Date'] = pd.to_datetime(df['Date'])
+df['Date'] = np.round(df['Date'],5)
 
-#define colors to use
-col1 = 'green'
-col2 = 'red'
+fig, ax = plt.subplots()
+candlestick_ohlc(ax,df.values , width=0.8, colorup='green', colordown='red')
 
-#plot up prices
-plt.bar(up.index,up.Close-up.Open,width,bottom=up.Open,color=col1)
-plt.bar(up.index,up.High-up.Close,width2,bottom=up.Close,color=col1)
-plt.bar(up.index,up.Low-up.Open,width2,bottom=up.Open,color=col1)
+ax.set_title('Gráfico de velas japonesas')
+ax.set_xlabel('Fecha')
+ax.set_ylabel('Precio')
 
-#plot down prices
-plt.bar(down.index,down.Close-down.Open,width,bottom=down.Open,color=col2)
-plt.bar(down.index,down.High-down.Open,width2,bottom=down.Open,color=col2)
-plt.bar(down.index,down.Low-down.Close,width2,bottom=down.Close,color=col2)
-
-#rotate x-axis tick labels
-plt.xticks(rotation=90, ha='right')
-
-#display candlestick chart
 plt.show()
