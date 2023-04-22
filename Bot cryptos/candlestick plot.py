3039b78b5afa17a31b68d192a04a90ffde715951
Galo0000 <guillermogallogarcia@gmail.com>
2023-04-22 18:00:34 -0300

    Update candlestick plot.py

--- Bot cryptos/candlestick plot.py
+++ Bot cryptos/candlestick plot.py
@@ -13,24 +13,24 @@ client = Client(USERBINANCE.API_KEY, USERBINANCE.API_SECRET, tld='com')
 basecoin = 'USDT'
 tradecoin = 'ETH'
 symbolTicker = tradecoin + basecoin
-klines = np.array(client.get_klines(symbol=symbolTicker, interval=Client.KLINE_INTERVAL_4HOUR,limit=100)).astype(np.float64)
+klines = np.array(client.get_klines(symbol=symbolTicker, interval=Client.KLINE_INTERVAL_4HOUR,limit=10)).astype(np.float64)
 
 def _sopres(df):
     val_high = df['High']
-    val_Low = df['Low']
-    sop = [df['Low'].loc[0]]
-    res = [df['High'].loc[0]]
+    #val_Low = df['Low']
+    #sop = [df['Low'].loc[0]]
+    res = [0]
     n = 0
     for value in val_high:
         if value > res[n]:
             res[n] = value
         else:
-            
-            if n <= len(res):
+            if n+1 == len(res) and value < res[n]:
+                res[n] = value
+                
+            else:
                 res.append(value)
                 n +=1
-            else:
-                res[n] = value
     return res
             
         
@@ -44,14 +44,14 @@ df['Date'] = np.round(df['Date'],5)
 
 
 
-
-y = _sopres(df)
-x = [df['Date'].loc[0],df['Date'].iloc[-1]]
+coord = _sopres(df)
+x = [df['Date'].iloc[0],df['Date'].iloc[-1]]
 
 fig, ax = plt.subplots()
 candlestick_ohlc(ax,df.values , width=10000, colorup='green', colordown='red')
 
-ax.plot(x, y)
+for a in coord:
+    ax.plot(x,[a,a])
 
 ax.set_title('Gráfico de velas japonesas')
 ax.set_xlabel('Fecha')
