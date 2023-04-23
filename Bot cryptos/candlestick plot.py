7efae096a3df3a5c6c481e66c328c921696db2b3
Galo0000 <guillermogallogarcia@gmail.com>
2023-04-23 16:23:51 -0300

    Update candlestick plot.py

--- Bot cryptos/candlestick plot.py
+++ Bot cryptos/candlestick plot.py
@@ -9,17 +9,33 @@ from binance.client import Client
 import numpy as np
 
 client = Client(USERBINANCE.API_KEY, USERBINANCE.API_SECRET, tld='com')
-nl = 200
+nl = 90
 basecoin = 'USDT'
 tradecoin = 'ETH'
 symbolTicker = tradecoin + basecoin
 klines = np.array(client.get_klines(symbol=symbolTicker, interval=Client.KLINE_INTERVAL_4HOUR,limit=nl)).astype(np.float64)
 
-def _sopres(df):
+
+def promedio_cercanos(lista, distancia):
+    promedio = 0
+    contador = 0
+    resultado = []
+    
+    for i in range(1, len(lista)-1):
+        if abs(lista[i]-lista[i-1]) <= distancia and abs(lista[i]-lista[i+1]) <= distancia:
+            promedio += lista[i]
+            contador += 1
+    
+    if contador > 0:
+        promedio /= contador
+        
+    resultado.append(promedio)
+    
+    return resultado
+
+def _res(df):
     global nl
     val_high = df['High'].astype(int)
-    #val_Low = df['Low']
-    #sop = [df['Low'].loc[0]]
     res = [0]*nl
     n = 0
     isup = False
@@ -41,11 +57,45 @@ def _sopres(df):
     for i in range(len(result)):
         result[i] = int(result[i] / 10) * 10
     
+    #result = promedio_cercanos(result,10)
+    
     resultfinal = []
     for n in result:
         if result.count(n) >= 3 and n not in resultfinal:
             resultfinal.append(n)
     
+    return resultfinal
+
+def _sop(df):
+    global nl
+    val_Low = df['Low'].astype(int)
+    res = [0]*nl
+    n = 0
+    isinf = False
+    temp = 0
+    result = []
+    for value in val_Low:
+        if value < temp:
+            res[n] = value
+            isinf = True
+        elif isinf:
+            isinf = False
+            n +=1
+        temp = value
+            
+    for a in res:
+        if a != 0:
+            result.append(a)
+            
+    for i in range(len(result)):
+        result[i] = int(result[i] / 10) * 10
+    
+    #result = promedio_cercanos(result,5)
+    
+    resultfinal = []
+    for n in result:
+        if result.count(n) >= 3 and n not in resultfinal:
+            resultfinal.append(n)
     
     return resultfinal
             
@@ -60,14 +110,20 @@ df['Date'] = np.round(df['Date'],5)
 
 
 
-coord = _sopres(df)
+resistencias = _res(df)
+soportes = _sop(df)
+
+
 x = [df['Date'].iloc[0],df['Date'].iloc[-1]]
 
 fig, ax = plt.subplots()
 candlestick_ohlc(ax,df.values , width=10000, colorup='green', colordown='red')
 
-for a in coord:
-    ax.plot(x,[a,a])
+for a in resistencias:
+    ax.plot(x,[a,a],color='red')
+    
+for a in soportes:
+    ax.plot(x,[a,a],color='green')
 
 ax.set_title('Gráfico de velas japonesas')
 ax.set_xlabel('Fecha')
