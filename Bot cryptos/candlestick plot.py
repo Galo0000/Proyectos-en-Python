fa3a2fc1e345e0bc63ef1caff9ae951aba588528
Galo0000 <guillermogallogarcia@gmail.com>
2023-04-27 16:40:25 -0300

    Update candlestick plot.py

--- Bot cryptos/candlestick plot.py
+++ Bot cryptos/candlestick plot.py
@@ -9,7 +9,7 @@ from binance.client import Client
 import numpy as np
 
 client = Client(USERBINANCE.API_KEY, USERBINANCE.API_SECRET, tld='com')
-nl = 100
+nl = 50
 basecoin = 'USDT'
 tradecoin = 'ETH'
 symbolTicker = tradecoin + basecoin
