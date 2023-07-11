a5e41e13f2bc084e89a097d42eef3c65b56e4631
Galo0000 <guillermogallogarcia@gmail.com>
2023-07-11 15:09:32 -0300

    Update candlestick plot.py

--- Bot cryptos/candlestick plot.py
+++ Bot cryptos/candlestick plot.py
@@ -1,12 +1,14 @@
 import sys
-sys.path.append( 'C:/Repositorios/Python' )
-sys.path.append( 'H:\Repositorios\Python no github\Bot cryptos' )
-import USERBINANCE
 import matplotlib.pyplot as plt
 import pandas as pd
 from mplfinance.original_flavor import candlestick_ohlc
 from binance.client import Client
 import numpy as np
+import time
+sys.path.append( 'C:/Repositorios/Python' )
+sys.path.append( 'H:\Repositorios\Python no github\Bot cryptos' )
+import USERBINANCE
+
 
 client = Client(USERBINANCE.API_KEY, USERBINANCE.API_SECRET, tld='com')
 nl = 100
@@ -15,8 +17,8 @@ cercania = 3
 basecoin = 'USDT'
 tradecoin = 'ETH'
 symbolTicker = tradecoin + basecoin
-klines = np.array(client.get_klines(symbol=symbolTicker, interval=Client.KLINE_INTERVAL_4HOUR ,limit=nl)).astype(np.float64)
 
+# Promedia los valores cercanos
 def _redo(l):
     prom = []
     complete = []
@@ -28,26 +30,10 @@ def _redo(l):
                     temp.append(b)
                     complete.append(b)
         if len(temp) > 2:
-            print(temp)
             prom.append(int(sum(temp)/len(temp)))
-            
-    #prom = []
-    #complete = []
-    #for a in l:
-    #    temp = []
-    #    for b in l:
-    #        if b not in temp:
-    #            inter = np.intersect1d(b, a)
-    #            if inter.size > 2:
-    #                temp.append(b)
-    #            else:
-    #                break
-    #    for r in temp:
-    #        arreglo_rango = np.array(r)
-    #        promedio_rango = np.mean(arreglo_rango)
-    #        complete.append(promedio_rango)
     return prom
 
+# Retorna los valores máximos del gráfico de velas
 def _res(df):
     val_high = df['High']
     val_o1 = df['Open'].astype(int)
@@ -69,6 +55,7 @@ def _res(df):
 
     return res
 
+# Retorna los valores mínimos del gráfico de velas
 def _sop(df):
     global nl,rep
     val_Low = df['Low']
@@ -90,35 +77,31 @@ def _sop(df):
     sop = _redo(sop)
     
     return sop
-            
-        
 
-
-
-
-df = pd.DataFrame(klines[:, :5],columns=['Date','Open','High','Low','Close'])
-#df['Date'] = pd.to_datetime(df['Date'])
-df['Date'] = np.round(df['Date'],5)
-
-
-
-resistencias = _res(df)
-soportes = _sop(df)
-
-
-x = [df['Date'].iloc[0],df['Date'].iloc[-1]]
-xcord = df['Date'].iloc[0]
 fig, ax = plt.subplots(figsize=(32, 24))
-candlestick_ohlc(ax,df.values , width=10000000, colorup='green', colordown='red')
 
-for a in resistencias:
-    ax.plot(x,[a,a],color='red')
-    ax.text(xcord, a,str(a), ha="left", va="bottom")
+while True:
+    klines = np.array(client.get_klines(symbol=symbolTicker, interval=Client.KLINE_INTERVAL_1HOUR, limit=nl)).astype(np.float64)
+    df = pd.DataFrame(klines[:, :5], columns=['Date','Open','High','Low','Close'])
+    df['Date'] = range(0,100)
     
-for a in soportes:
-    ax.plot(x,[a,a],color='green')
-    ax.text(xcord, a,str(a), ha="left", va="bottom")
-
-ax.set_title('ETHUSDT')
-
-plt.show()
+    resistencias = _res(df)
+    soportes = _sop(df)
+    
+    x = [df['Date'].iloc[0], df['Date'].iloc[-1]]
+    xcord = df['Date'].iloc[0]
+    
+    ax.clear()
+    candlestick_ohlc(ax, df.values, width=0.6, colorup='green', colordown='red')
+    
+    for a in resistencias:
+        ax.plot(x, [a, a], color='red')
+        ax.text(xcord, a, str(a), ha="left", va="bottom")
+        
+    for a in soportes:
+        ax.plot(x, [a, a], color='green')
+        ax.text(xcord, a, str(a), ha="left", va="bottom")
+    
+    ax.set_title('ETHUSDT')
+    
+    plt.pause(5)
