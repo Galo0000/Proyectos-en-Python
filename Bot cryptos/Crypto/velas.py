d89f1d6a3dcabab6fe6e808ff2740a421c968f23
Galo0000 <guillermogallogarcia@gmail.com>
2023-10-14 16:09:05 -0300

    up

--- Bot cryptos/Crypto/velas.py
+++ Bot cryptos/Crypto/velas.py
@@ -1,4 +1,5 @@
-import sys
+import userbinance
+import matplotlib.pyplot as plt
 import userbinance
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -7,43 +8,54 @@ from binance.client import Client
 import numpy as np
 from matplotlib.animation import FuncAnimation
 
-# Initialize Binance client
+# Inicializar el cliente de Binance
 client = Client(userbinance.API_KEY, userbinance.API_SECRET, tld='com')
 
-# Parameters
+# Parámetros
 nl = 100
-basecoin = 'USDT'
-tradecoin = 'ETH'
-symbolTicker = tradecoin + basecoin
-interval = Client.KLINE_INTERVAL_4HOUR
+moneda_base = 'USDT'
+moneda_negociada = 'ETH'
+simbolo = moneda_negociada + moneda_base
+intervalo = Client.KLINE_INTERVAL_1HOUR
 
-# Function to fetch data and process
-def fetch_and_process_data():
-    klines = np.array(client.get_klines(symbol=symbolTicker, interval=interval, limit=nl)).astype(np.float64)
-    df = pd.DataFrame(klines[:, :5], columns=['Date', 'Open', 'High', 'Low', 'Close'])
-    df['Date'] = np.round(df['Date'], 5)
+# Función para obtener y procesar los datos
+def obtener_y_procesar_datos():
+    klines = np.array(client.get_klines(symbol=simbolo, interval=intervalo, limit=nl)).astype(np.float64)
+    df = pd.DataFrame(klines[:, :5], columns=['Fecha', 'Apertura', 'Máximo', 'Mínimo', 'Cierre'])
+    df['Fecha'] = np.round(df['Fecha'], 5)
     return df
 
+# Función para actualizar el gráfico
+def actualizar_grafico(i):
+    df = obtener_y_procesar_datos()
 
-# Function to update the plot
-def update_plot(i):
-    df = fetch_and_process_data()
+    x = [df['Fecha'].iloc[0], df['Fecha'].iloc[-1]]
+    xcord = df['Fecha'].iloc[0]
 
-    x = [df['Date'].iloc[0], df['Date'].iloc[-1]]
-    xcord = df['Date'].iloc[0]
-
-    # Clear previous plot
+    # Limpiar el gráfico previo
     ax.clear()
 
-    # Plot the new data
-    candlestick_ohlc(ax, df.values, width=10000000, colorup='green', colordown='red')
+    # Graficar los nuevos datos
+    candlestick_ohlc(ax, df.values, width=2000000, colorup='green', colordown='red')
 
     ax.set_title('ETHUSDT')
 
-# Set up the figure and axis
+    # Obtener el último precio de cierre
+    ultimo_cierre = df['Cierre'].iloc[-1]
+
+    # Dibujar una línea vertical en el último precio de cierre
+    ax.axhline(ultimo_cierre, color='black', linestyle='-')
+
+    # Dibujar una línea vertical en el último precio de cierre
+    ax.axhline(1600, color='yellow', linestyle='-')
+
+    # Mostrar el valor del último precio de cierre
+    ax.text(xcord, ultimo_cierre, ultimo_cierre, ha='left', va='bottom')
+
+# Configurar la figura y el eje
 fig, ax = plt.subplots(figsize=(32, 24))
 
-# Set up the animation
-ani = FuncAnimation(fig, update_plot, interval=1000)  # Update every 10 seconds (10000 milliseconds)
+# Configurar la animación
+ani = FuncAnimation(fig, actualizar_grafico, interval=100, save_count=nl)
 
 plt.show()
